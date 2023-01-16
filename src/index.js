fe2c7131c8433ba14a0497296ce36cff4477bdb4
Maksim Egorov <maxceas228@gmail.com>
2023-01-16 10:46:30 +0300

    init: function works before 19

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function toReadable (number) {
+    
+    const ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']; 
+    const secondTens = ['ten', 'eleven', 'twelve', 'thirteen','fourteen', 'fifteen', 'sixteen', 'seventeen','eighteen', 'nineteen'];
+    const tens = ['ten', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const hundred = 'hundred';
+    let razryad = number.toString()[0]; // 2
+    const str = number.toString()    
+    if (str.length == 1) {
+        return ones[number]
+    }
+    
+    if (str.length == 2 && number <= 19) {
+        return secondTens[number - 10];
+    }
+
+    // if (str.length == 2 && number >= 20 && number <= 99 && razryad === tens[razryad])
+    // return 
+    
+} 
+    console.log(module.exports(20))
   
-}
